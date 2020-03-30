89de7dd46446235b4873e37777cbda0404ccd102
CarlosAkel <cmakel@miuandes.cl>
2020-03-30 10:45:56 -0300

    Todo listo al parecer

--- Musica/Espotifai.cs
+++ Musica/Espotifai.cs
@@ -7,28 +7,20 @@ using System.Text;
 
 namespace Musica
 {
-    class Espotifai 
+    class Espotifai
     {
-        //public string song { get; set; }
-        /*public List<string> Rep { get; set; }*/
+
         List<Cancion> ListaMusica = new List<Cancion>();
         List<Cancion> songsList = new List<Cancion>();
+        List<Cancion> Play = new List<Cancion>();
+        List<string> myStrings = new List<string>();
 
 
 
-        
 
-        public bool  AddSong(Cancion cancion)
+        public bool AddSong(Cancion cancion)
         {
-            //List<Espotifai> ListaMusica = new List<Espotifai>();
-            /* Console.WriteLine("genero");
-             cancion.genero = Console.ReadLine();
-             Console.WriteLine("artista");
-             cancion.artista = Console.ReadLine();
-             Console.WriteLine("album");
-             cancion.album = Console.ReadLine();
-             Console.WriteLine("nombre");
-             cancion.nombre = Console.ReadLine();*/
+
             foreach (Cancion item in ListaMusica)
             {
                 if (item.Tipo("genero") == cancion.Tipo("genero") & item.Tipo("album") == cancion.Tipo("album") & item.Tipo("artista") == cancion.Tipo("artista") & item.Tipo("nombre") == cancion.Tipo("nombre"))
@@ -40,42 +32,18 @@ namespace Musica
             }
             ListaMusica.Add(cancion);
             return true;
-                /*if (ListaMusica.Contains(cancion))
-            {
-                return false;
-            }
-            else
-            {
-                ListaMusica.Add(cancion);
-                return true;
-            }*/
-
-
-
-            //ListaMusica.Add(new Espotifai());
-
-            /*foreach (var author in ListaMusica)
-            {
-                Console.WriteLine("datos",cancion.genero,cancion.artista,cancion.album,cancion.nombre);
-            }
-            return true;*/
 
         }
         public void ShowSongs()
         {
-            foreach(Cancion item in ListaMusica)
+            foreach (Cancion item in ListaMusica)
             {
-                Console.WriteLine(item.Tipo("genero"));
-                Console.WriteLine(item.Tipo("album"));
-                Console.WriteLine(item.Tipo("artista"));
-                Console.WriteLine(item.Tipo("nombre"));
+                Console.WriteLine(item.informacion());
+                Console.WriteLine("-------");
             }
-            /*Console.WriteLine(ListaMusica[0].genero);
-            Console.WriteLine(ListaMusica[0].album);
-            Console.WriteLine(ListaMusica.Count);*/
 
 
-            }
+        }
         public List<Cancion> CancionesPorCriterio(String criterio, String valor)
         {
             //algo que borre la lista
@@ -104,5 +72,75 @@ namespace Musica
 
 
         }
+        public bool GenerarPlaylist(String criterio, String valorCriterio, String nombrePlaylist)
+        {
+            if(criterio == "genero" || criterio == "album" || criterio == "artista" || criterio == "nombre")
+            {
+                Console.WriteLine("ok");
+            }
+            else
+            {
+                Console.WriteLine("No se pudo ejecutar, intentelo denuevo");
+                return false;
+            }
+
+            PlayList uno = new PlayList(criterio, valorCriterio, nombrePlaylist);
+                if (uno.Listas().Contains(uno))
+                {
+                Console.WriteLine("No se pudo ejecutar, intentelo denuevo");
+                return false;
+                }
+            uno.añadir(uno);
+            Console.WriteLine(uno.Listas()[0].Datos("nombrePlaylist"));
+            //myStrings.Clear();//revisar
+            myStrings.Add(uno.ShowSongs2());
+
+
+
+
+            foreach (Cancion item2 in ListaMusica)
+            {
+                if (item2.Tipo("genero") == valorCriterio)
+                {
+                    Play.Add(item2);
+                    Console.WriteLine("si");
+                    return true;
+                }
+                else if (item2.Tipo("album") == valorCriterio)
+                {
+                    Play.Add(item2);
+                    return true;
+                }
+                else if (item2.Tipo("artista") == valorCriterio)
+                {
+                    Play.Add(item2);
+                    return true;
+                }
+                else if (item2.Tipo("nombre") == valorCriterio)
+                {
+                    Play.Add(item2);
+                    return true;
+                }
+            }
+            Console.WriteLine("No se pudo ejecutar, intentelo denuevo");
+            return false;
+        }
+        public String VerMisPlaylists()
+        {
+            int i = 0;
+            PlayList uno = new PlayList("ha", "ha", "ha");
+            foreach (Cancion item in Play)
+            {
+                Console.WriteLine(myStrings[i]);
+                Console.WriteLine(item.Tipo("genero"));
+                Console.WriteLine(item.Tipo("album"));
+                Console.WriteLine(item.Tipo("artista"));
+                Console.WriteLine(item.Tipo("nombre"));
+                Console.WriteLine("-------");
+                i++;
+            }
+            return " ";
+        }
+
     }
 }
---
+++ Musica/PlayList.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Musica
+{
+    class PlayList 
+    {
+        private string criterio { get; set; }
+        private string valorCriterio { get; set; }
+        private string nombrePlaylist { get; set; }
+
+        List<PlayList> ListaRep = new List<PlayList>();
+
+
+        public string ShowSongs2()
+        {
+            foreach (PlayList item in ListaRep)
+            {
+
+                return item.Datos("nombrePlaylist");
+
+            }
+            return "-------";
+        }
+        public PlayList(string criterio, string valorCriterio, string nombrePlaylist)
+        {
+            this.criterio = criterio;
+            this.valorCriterio = valorCriterio;
+            this.nombrePlaylist = nombrePlaylist;
+        }
+
+
+     
+
+        public string Datos(string datos)
+        {
+            if (datos == "criterio")
+            {
+                return criterio;
+            }
+            else if (datos == "valorCriterio")
+            {
+                return valorCriterio;
+            }
+            else if (datos == "nombrePlaylist")
+            {
+                return nombrePlaylist;
+            }
+            return "0";
+        }
+        public List<PlayList> Listas()
+        {
+            return ListaRep;
+        }
+        public bool añadir(PlayList cosa)
+        {
+            ListaRep.Add(cosa);
+            return true;
+        }
+    }
+
+    
+}
